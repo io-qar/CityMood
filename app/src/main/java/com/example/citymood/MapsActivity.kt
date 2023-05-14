96f762c26ff968f83144e3d2f229ae1112ac8bb6
io-qar <koroold@gmail.com>
2023-05-14 22:54:10 +0300

    fixed map, added zoom

--- app/src/main/java/com/example/citymood/MapsActivity.kt
+++ app/src/main/java/com/example/citymood/MapsActivity.kt
@@ -1,33 +1,22 @@
 package com.example.citymood
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import androidx.appcompat.app.AppCompatActivity
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
-import com.example.citymood.databinding.ActivityMapsBinding
 
-class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
-	private lateinit var mMap: GoogleMap
-	private lateinit var binding: ActivityMapsBinding
-
+internal class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.activity_maps)
 
-		binding = ActivityMapsBinding.inflate(layoutInflater)
-		setContentView(binding.root)
-
-		// Obtain the SupportMapFragment and get notified when the map is ready to be used.
-		val mapFragment = supportFragmentManager
-			.findFragmentById(R.id.map) as SupportMapFragment
-		mapFragment.getMapAsync(this)
-
+		val mapFragment = supportFragmentManager.findFragmentById(R.id.fragments) as? SupportMapFragment
+		mapFragment?.getMapAsync(this)
 	}
 
 	/**
@@ -40,11 +29,16 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 	 * installed Google Play services and returned to the app.
 	 */
 	override fun onMapReady(googleMap: GoogleMap) {
-		mMap = googleMap
-
+		googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL)
+		val loc = LatLng(-33.852, 151.211)
 		// Add a marker in Sydney and move the camera
-		val sydney = LatLng(-34.0, 151.0)
-		mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-		mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+		googleMap.addMarker(
+			MarkerOptions()
+				.position(loc)
+				.title("Marker")
+				.draggable(true)
+		)
+		googleMap.moveCamera(CameraUpdateFactory.newLatLng(loc))
+		googleMap.moveCamera(CameraUpdateFactory.zoomTo(10F))
 	}
 }
